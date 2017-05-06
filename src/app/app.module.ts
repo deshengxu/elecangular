283cebba18ab169a31d6879a1c93459493799d23
xudesheng <xudesheng@yahoo.com>
2017-05-06 15:32:58 -0400

    add save function with in memory web api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,21 @@ import { AppComponent }   from './app.component';
 import { HeroDetailComponent } from "./hero-detail.component";
 import { HeroService } from "./service/hero.service";
 import { HeroesComponent } from "./heroes.component";
+import { HttpModule }   from '@angular/http';
 
 import { RouterModule, Routes }   from '@angular/router';
 import { DashboardComponent } from "./dashboard.component";
 import { AppRoutingModule } from "./app-routing.module";
 import {APP_BASE_HREF} from '@angular/common';
 
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 @NgModule({
   imports:      [ BrowserModule,
     FormsModule,
+    HttpModule,
+    //InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [ AppComponent,
--- src/app/service/hero.service.ts
+++ src/app/service/hero.service.ts
@@ -1,16 +1,41 @@
 import { Injectable }           from '@angular/core';
 import { Hero }                 from '../db/hero';
-import { HEROES }               from "./../db/mock-heroes";
+//import { HEROES }               from "./../db/mock-heroes";
+import { Headers, Http }    from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class HeroService{
+
+    HEROES: Hero[] = [
+        {id: 11, name: 'Mr. Nice'},
+        {id: 12, name: 'Narco'},
+        {id: 13, name: 'Bombasto'},
+        {id: 14, name: 'Celeritas'},
+        {id: 15, name: 'Magneta'},
+        {id: 16, name: 'RubberMan'},
+        {id: 17, name: 'Dynama'},
+        {id: 18, name: 'Dr IQ'},
+        {id: 19, name: 'Magma'},
+        {id: 20, name: 'Tornado'}
+    ];
+
     getHeroes():Promise<Hero[]> {
-        return Promise.resolve(HEROES);
+        return Promise.resolve(this.HEROES);
     }
 
     getHero(id: number): Promise<Hero> {
         return this.getHeroes()
              .then(heroes => heroes.find(hero => hero.id === id));
+    }
+
+    update(hero: Hero): Promise<Hero>{
+        this.HEROES.forEach(function(part, index, theArray){
+            if(part.id == hero.id){
+                theArray[index] = hero;
+            }
+        });
+        return Promise.resolve(hero);
     }
 
 }
